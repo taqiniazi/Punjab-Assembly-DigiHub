eb0aba450ac2fa78f370d98cc06c15f654cd3848
Taqi Raza <taqi.raza@pitb.gov.pk>
2026-01-03 20:37:50 +0500

    modal update

--- src/app/components/member-assembly-dashboard/member-assembly-dashboard.component.ts
+++ src/app/components/member-assembly-dashboard/member-assembly-dashboard.component.ts
@@ -12,9 +12,14 @@ export class MemberAssemblyDashboardComponent {
   AgendaIcon = './assets/images/today_agenda.svg';
   notificationIcon = './assets/images/notificationIcon.svg';
   questionsIcon = './assets/images/questionsIcon.svg';
+  selectedImages: string[] = [];
   assembly_dashboard_cards =[
-    {title: "Agenda", icon: this.AgendaIcon , class : "agenda"},
-    {title: "Notifications", icon: this.notificationIcon , class : "notifications"},
-    {title: "Questions", icon: this.questionsIcon , class : "questions underline"}
-  ]
+    {title: "Agenda", icon: this.AgendaIcon , class : "agenda", images: ['./assets/images/gallary/1.png','./assets/images/gallary/2.png','./assets/images/gallary/3.png']},
+    {title: "Notifications", icon: this.notificationIcon , class : "notifications", images: ['./assets/images/gallary/3.png','./assets/images/gallary/2.png','./assets/images/gallary/1.png']},
+    {title: "Questions", icon: this.questionsIcon , class : "questions underline", images: ['./assets/images/gallary/2.png','./assets/images/gallary/3.png','./assets/images/gallary/2.png']}
+  ];
+
+  openModal(item: any) {
+    this.selectedImages = item.images;
+  }
 }
