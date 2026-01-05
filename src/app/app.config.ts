7a23b46307bc93cff6506ddbd3775931976c6b70
Taqi Raza <taqi.raza@pitb.gov.pk>
2026-01-05 12:17:17 +0500

    add lazy loading

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withPreloading, PreloadAllModules } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()), provideClientHydration()]
+  providers: [provideRouter(routes, withComponentInputBinding(), withPreloading(PreloadAllModules)), provideClientHydration()]
 };
